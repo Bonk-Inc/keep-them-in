2a235c33a386abad974b71e7fa58d5863c6e81f1
Rico Evers <ricoevers@live.nl>
2020-07-12 14:52:01 +0200

    Guard AI working

--- Assets/Scripts/Movement/PawnMovement.cs
+++ Assets/Scripts/Movement/PawnMovement.cs
@@ -5,15 +5,22 @@ public class PawnMovement : MonoBehaviour
 {
     [SerializeField]
     private NavMeshAgent navMeshAgent;
+    [SerializeField]
     private Vector3 fixedDestination;
+    [SerializeField]
+    private bool randomDestenation = true;
 
     private void Awake() {
+        if(randomDestenation) {
         fixedDestination = new Vector3(Random.Range(-100, 100), transform.position.y, Random.Range(-100, 100));
         fixedDestination = fixedDestination.normalized * 50;
+        } else {
+            fixedDestination = transform.position;
+        }
     }
 
     private void FixedUpdate() {
-        if (navMeshAgent.remainingDistance < .5f) {
+        if (navMeshAgent.remainingDistance < .2f) {
             GoToDestenation();
         }
     }
