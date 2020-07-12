082583733046e3d62e161f58fa50cea78d9bd089
Coen Steuns <c.steuns@gmail.com>
2020-07-12 19:24:21 +0200

    simple camera movement

---
+++ Assets/Scripts/CameraMovement/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.01f;
+
+    private Vector2 prevMousePosition = Vector2.zero;
+    private Vector2 mouseDelta = Vector2.zero;
+
+    [SerializeField]
+    private WorldBounds bounds;
+
+    private void Awake()
+    {
+        prevMousePosition = Input.mousePosition;
+        bounds.CalculateBounds();
+    }
+
+    private void Update()
+    {
+        UpdateMouseinfo();
+        MoveCamera();
+    }
+
+    private void UpdateMouseinfo()
+    {
+        mouseDelta = prevMousePosition - (Vector2)Input.mousePosition;
+        prevMousePosition = Input.mousePosition;
+    }
+
+    private void MoveCamera()
+    {
+        if (!Input.GetMouseButton(2))
+            return;
+        var movement = new Vector3(mouseDelta.x, 0, mouseDelta.y) * speed;
+        var newPosition = transform.position;
+        newPosition += movement;
+        newPosition = bounds.ClampPosition(newPosition);
+        transform.position = newPosition;
+
+    }
+
+
+
+
+}
