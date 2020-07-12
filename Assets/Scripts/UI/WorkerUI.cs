ee8c229384e773bcc35246b564db4f9c50985d2a
Coen Steuns <c.steuns@gmail.com>
2020-07-12 20:44:14 +0200

    worker ui updating

---
+++ Assets/Scripts/UI/WorkerUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private TMPro.TextMeshProUGUI maxAvailibleUI, availibleUI;
+
+    [SerializeField]
+    private BuildManager manager;
+    private void Start()
+    {
+        SetAvailibleUI(manager.MaxAvailableWorkers);
+        SetMaxAvailibleUI(manager.AvailableWorkers);
+        manager.OnAvailableWorkersChange += SetAvailibleUI;
+        manager.OnMaxAvailableWorkersChange += SetMaxAvailibleUI;
+    }
+
+    private void SetMaxAvailibleUI(int amount)
+    {
+        maxAvailibleUI.text = amount.ToString();
+    }
+
+    private void SetAvailibleUI(int amount)
+    {
+        availibleUI.text = amount.ToString();
+    }
+
+}
